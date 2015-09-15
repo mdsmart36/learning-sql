879bfae7264e07e93837c8eb418cd221e0370dbd
Matt Smart <mdsmart36@gmail.com>
2015-09-15 14:41:30 -0500

    added queries 8 - 10

---
+++ 10.sql
@@ -0,0 +1,1 @@
+SELECT COUNT(*) AS [Number of line items for Invoice ID 37] FROM (SELECT * FROM InvoiceLine WHERE InvoiceId == '37')
--- 8.sql
+++ 8.sql
@@ -1,4 +1,4 @@
-SELECT i.Total, c.FirstName, c.LastName, c.Country, e.FirstName, e.LastName FROM Invoice i
+SELECT i.Total, c.FirstName, c.LastName, c.Country, e.FirstName as [Sales Rep First Name], e.LastName as [Sales Rep Last Name] FROM Invoice i
 JOIN Customer c ON i.CustomerId == c.CustomerId
 JOIN Employee e ON c.SupportRepId == e.EmployeeId
 ORDER By e.FirstName, e.LastName
